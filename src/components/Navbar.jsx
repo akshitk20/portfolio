a6d640f2c1291028d82c0d9da6c415113f4f932d
akshitk20 <akshitkhatri.20@gmail.com>
2026-04-03 19:27:40 +0530

    updated resume section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <a href="#" className="navbar-logo">AK</a>
+        <div className="navbar-links">
+          <a href="#about">About</a>
+          <a href="#skills">Skills</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+          <a href="/resume.pdf" download className="navbar-resume">
+            Resume
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
